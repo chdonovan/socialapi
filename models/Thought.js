ce2efc9fc46f0a4f0908d8b2cdf5075d7a4f082d
chdonovan <chrisdonovan311@gmail.com>
2021-07-15 19:47:26 -0500

    reatcion schema

--- models/Thought.js
+++ models/Thought.js
@@ -23,3 +23,29 @@ const ReactionSchema = new Schema(
         }
     }
 ); 
+const ThoughtSchema = new Schema(
+    {
+        thoughtText: {
+            type: String,
+            required: true,
+            minLength: 1,
+            maxLength: 280
+        },
+        createdAt: {
+            type: Date,
+            default: Date.now,
+            get: (createdAtVal) => dateFormat(createdAtVal)
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        reactions: [ReactionSchema]
+    },
+    {
+        toJSON: {
+            virtuals: true,
+            getters: true
+        }
+    }
+);
