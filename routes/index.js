dcd9e066db940b083c089073b3886db21794b969
chdonovan <chrisdonovan311@gmail.com>
2021-07-17 10:04:25 -0500

    routes index

---
+++ routes/index.js
@@ -0,0 +1,12 @@
+// inport routes
+const router = require('express').Router();
+const apiRoutes = require('./api');
+
+router.use('/api', apiRoutes);
+
+// 404 for unresolved route
+router.use(( req, res) => {
+    res.status(404).send('<h1> 404 error </h1>')
+});
+
+module.exports = router;
